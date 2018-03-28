550b617346a7ac2a6f81a3dfc670191ad8449b63
OmniproEdge <OmniproEdge_Software_Service_pvt.ltd@OmniProEdges-iMac.local>
2018-03-28 13:19:00 +0530

    Add Pod Files

--- PayUMoney/ViewController.swift
+++ PayUMoney/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum TestPayment : String {
+        
+        case merchantID     = "396132"
+        case merchantkey    = "40747T"
+        case baseURL        = "https://test.payu.in"
+        case salt           = "cJHb2BC9"
+        
+    }
+    
+    let testParam : PUMTxnParam = PUMTxnParam()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        testParam.amount = "100"
+        testParam.email = "bala@yopmail.com"
+        testParam.firstname = "bala"
+        testParam.txnID = "ga7di6ft8qwasd"
+        testParam.phone = "8148222948"
+        testParam.furl = "https://www.payumoney.com/mobileapp/payumoney/failure.php"
+        testParam.surl = "https://www.payumoney.com/mobileapp/payumoney/success.php"
+        testParam.productInfo = "iphone X"
+        testParam.environment = .test
+        
+        testParam.key = TestPayment.merchantkey.rawValue
+        testParam.merchantid = TestPayment.merchantID.rawValue
+        
+        testParam.udf1 = ""
+        testParam.udf2 = ""
+        testParam.udf3 = ""
+        testParam.udf4 = ""
+        testParam.udf5 = ""
+        testParam.udf6 = ""
+        testParam.udf7 = ""
+        testParam.udf8 = ""
+        testParam.udf9 = ""
+        testParam.udf10 = ""
+        
+        let hashValue = String.localizedStringWithFormat("%@|%@|%@|%@|%@|%@|||||||||||%@",TestPayment.merchantkey.rawValue,testParam.txnID!,testParam.amount!,testParam.productInfo!,testParam.firstname!,testParam.email!,TestPayment.salt.rawValue)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        testParam.hashValue = hashValue.sha512()      // need to convert sha
+        
+        PlugNPlay.presentPaymentViewController(withTxnParams: testParam, on: self) { (responce, error, otherParam) in
+            print(responce as Any)
+            print("---------------------------------------------")
+            print(error as Any)
+            print("---------------------------------------------")
+            print(otherParam as Any)
+
+        }
     }
 
 
 }
 
+extension String {
+    
+    func sha512() -> String {
+        let data = self.data(using: .utf8)!
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA512_DIGEST_LENGTH))
+        data.withUnsafeBytes({
+            _ = CC_SHA512($0, CC_LONG(data.count), &digest)
+        })
+        return digest.map({ String(format: "%02hhx", $0) }).joined(separator: "")
+    }
+    
+}
